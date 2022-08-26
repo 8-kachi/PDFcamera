99d39269743f6abdc1f291cef94723744c44d908
8-kachi <ponta510000@gmail.com>
2022-08-27 08:04:27 +0900

    Initial Commit

---
+++ PDFcamera/PDFcameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFcameraApp.swift
+//  PDFcamera
+//
+//  Created by 浅野総一郎 on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct PDFcameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
